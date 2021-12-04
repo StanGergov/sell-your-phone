987f5b7b9fe4ba38eecf1f08dcfdf7c0957a4580
StanGergov <81183207+StanGergov@users.noreply.github.com>
2021-12-04 19:29:54 +0200

    Add Register page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/HeaderComponent/Header";
 import Home from './components/HomeComponent/Home';
 import AllPhones from './components/AllPhones/AllPhones';
 import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 import Footer from './components/FooterComponent/Footer';
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
 
      {/* <Home /> */}
      {/* <AllPhones /> */}
-     <Login />
+     {/* <Login /> */}
+     <Register />
 
      <Footer />
     </div>
