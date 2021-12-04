504ee30529d1e972d7d54622aa91e70ea70105dc
StanGergov <81183207+StanGergov@users.noreply.github.com>
2021-12-04 17:05:04 +0200

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from "./components/HeaderComponent/Header";
+import Home from './components/HomeComponent/Home';
 import Footer from './components/FooterComponent/Footer';
 
 function App() {
   return (
     <div className="App">
      <Header />
+
+     <Home />
 
      <Footer />
     </div>
---
+++ src/components/HomeComponent/Home.js
@@ -0,0 +1,39 @@
+import { Carousel, Button } from 'react-bootstrap';
+
+import './Home.css';
+
+const Home = () => {
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="../../../banner.jpg"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3 className="slide-title">Sell your phone</h3>
+                    {/* <p>If you want to sell your phone, create your sales ad.</p>
+                    <Button variant="danger">Create ad</Button> */}
+                    <p>If you want to sell your phone, you need to login first.</p>
+                    <Button variant="danger">Login</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="../../../banner.jpg"
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3 className="slide-title">Buy a phone</h3>
+                    <p>If you want to buy a phone, check what our users have to offer.</p>
+                    <Button variant="danger">Check here</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+};
+
+export default Home;
