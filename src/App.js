d51638c1959d2274cc9636fc34987c646939a2f9
StanGergov <81183207+StanGergov@users.noreply.github.com>
2021-12-05 18:57:33 +0200

    Add create page

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import AllPhones from './components/AllPhonesComponent/AllPhones';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Footer from './components/FooterComponent/Footer';
+import Create from './components/CreateComponent/Create';
 
 function App() {
   return (
     <div className="App">
       <Header />
-        
+
       <Switch>
 
         < Route path="/" exact component={Home} />
         < Route path="/allphones" component={AllPhones} />
+        < Route path="/create" component={Create} />
         < Route path="/login" component={Login} />
         < Route path="/register" component={Register} />
 
---
+++ src/components/CreateComponent/Create.js
@@ -0,0 +1,95 @@
+import { Form, Button } from 'react-bootstrap';
+
+import './Create.css';
+
+
+const Create = () => {
+
+    const createNewAd = (e) => {
+        e.preventDefault()
+
+        const formDate = new FormData(e.target);
+        console.log(Object.fromEntries(formDate));
+    }
+
+    return (
+        <>
+            <h1 className="page-title">Create an ad for your phone</h1>
+            <Form className="ad-form" onSubmit={ createNewAd }>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Brand</Form.Label>
+                    <Form.Control type="text" name="Brand" placeholder="Samsung, Apple, Sony, Nokia..." />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Model</Form.Label>
+                    <Form.Control type="text" name="model" placeholder="Galaxy Fold 3, iPhone 13, Xperia Z4..." />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Color</Form.Label>
+                    <Form.Control type="text" name="color" placeholder="Black, White, Green...." />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Image</Form.Label>
+                    <Form.Control type="text" name="imgUrl" placeholder="Image URL..." />
+                </Form.Group>
+
+                <Form.Label>Grade of conditions</Form.Label>
+                <Form.Select aria-label="Default select example" name="grade">
+                    <option>Select a value</option>
+                    <option value="1 Lots of scratches but still working">1 Lots of scratches but still working</option>
+                    <option value="2 Some scratches">2 Some scratches</option>
+                    <option value="3 Normal, with a few scratches">3 Normal, with a few scratches</option>
+                    <option value="4 Almost like new">4 Almost like new</option>
+                    <option value="5 New">5 New</option>
+                </Form.Select>
+
+                <Form.Label name="accessories">Accessories</Form.Label>
+                <div className="mb-3">
+                    <Form.Check
+                        inline
+                        label="Box"
+                        name="box"
+                        type="checkbox"
+                        id="box"
+                    />
+                    <Form.Check
+                        inline
+                        label="Charger"
+                        name="charger"
+                        type="checkbox"
+                        id="charger"
+                    />
+                    <Form.Check
+                        inline
+                        label="Cable"
+                        type="checkbox"
+                        name="cable"
+                        id="cable"
+                    />
+                    <Form.Check
+                        inline
+                        label="Headphones"
+                        name="headphones"
+                        type="checkbox"
+                        id="headphones"
+                    />
+                </div>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Notes</Form.Label>
+                    <Form.Control type="text" name="notes" placeholder="Notes" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Price</Form.Label>
+                    <Form.Control type="number" name="price" placeholder="500lv..." />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Create
+                </Button>
+            </Form>
+        </>
+    );
+};
+
+export default Create;
